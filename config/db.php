a8618726410532b724104d90169fb8a94d2d42a0
pabloaaab <paul6126@hotmail.com>
2018-12-28 12:10:13 -0500

    bd

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=bdescuelacopia',
+    'dsn' => 'mysql:host=localhost;dbname=bdescuelanueva',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
